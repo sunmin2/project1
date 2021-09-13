d33518931068a0ad47e25214f7baf3b6313d76ed
sunmin2 <90298604+sunmin2@users.noreply.github.com>
2021-09-13 10:19:54 +0900

    Add files via upload

---
+++ 방탈출 기획서.cpp
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main() {
+	int 방탈출; 
+	printf("%d", 주차장에서 힌트를 모아 문제를 풀면서 P2층에서부터 P1층 까지 가면서 P1층에서 탈출을한다.);
+	
+	printf("%d", 주차장 구조 및 물건 힌트.);
+	
+	printf("%d", 힌트);
+	ruturn 0;
+}
+ 
+
